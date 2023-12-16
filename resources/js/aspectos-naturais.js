49d6e8fc1ccf816758487f9e6c07272df59a00a6
ja1za1 <jlterrafarias@gmail.com>
2023-12-16 12:15:05 -0300

    initial commit

---
+++ resources/js/aspectos-naturais.js
@@ -0,0 +1,8 @@
+ScrollReveal().reveal(".texto-aspectos-naturais", {
+    delay: 250,
+    duration: 600,
+    easing: 'ease',
+    reset: true,
+    origin: 'bottom',
+    distance: "2rem",
+})
---
+++ rodar-webpage.sh
@@ -0,0 +1,1 @@
+chromium --kiosk --noerrdialogs --enable-features=OverlayScrollbar --disable-features=Translate --disable-restore-session-state http://127.0.0.1:5500
