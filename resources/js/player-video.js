87872ac5c5b8fcb921da87eae019bcfab0257c6c
ja1za1 <jlterrafarias@gmail.com>
2024-04-10 02:37:45 -0300

    Changed player-video file to load on page loading

--- resources/js/player-video.js
+++ resources/js/player-video.js
@@ -1,20 +1,23 @@
-const videos = document.getElementsByTagName('video')
 
-for (let video of videos) {
-    video.addEventListener('click', () => {
-        if (video.paused) {
-            /* if video is currently paused, play it */
-            video.play();
-        } else {
-            /* video isn't paused... */
-            if (video.ended) {
-                /* if we're at the end, reset currentTime and play */
-                video.currentTime=0;
+
+window.onload = function() {
+    let videos = document.getElementsByTagName('video')
+    for (let video of videos) {
+        video.addEventListener('click', () => {
+            if (video.paused) {
+                /* if video is currently paused, play it */
                 video.play();
             } else {
-                /* otherwise, just pause */
-                video.pause();
+                /* video isn't paused... */
+                if (video.ended) {
+                    /* if we're at the end, reset currentTime and play */
+                    video.currentTime=0;
+                    video.play();
+                } else {
+                    /* otherwise, just pause */
+                    video.pause();
+                }
             }
-        }
-    })
+        })
+    }
 }
